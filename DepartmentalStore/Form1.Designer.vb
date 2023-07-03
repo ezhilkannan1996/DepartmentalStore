cee559f10898c77c426bde5a911c81ddefb646fd
ezhil.k@swordgroup.in <ezhil.k@swordgroup.in>
2023-07-03 18:07:11 +0530

    Add project files.

---
+++ DepartmentalStore/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UserGrid = New System.Windows.Forms.DataGridView()
+        Me.BtnNew = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.BtnRefresh = New System.Windows.Forms.Button()
+        Me.BtnRemove = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BtnSearch = New System.Windows.Forms.Button()
+        CType(Me.UserGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UserGrid
+        '
+        Me.UserGrid.AllowUserToAddRows = False
+        Me.UserGrid.AllowUserToDeleteRows = False
+        Me.UserGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.UserGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.UserGrid.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.UserGrid.Location = New System.Drawing.Point(0, 51)
+        Me.UserGrid.Name = "UserGrid"
+        Me.UserGrid.ReadOnly = True
+        Me.UserGrid.Size = New System.Drawing.Size(800, 372)
+        Me.UserGrid.TabIndex = 0
+        '
+        'BtnNew
+        '
+        Me.BtnNew.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.BtnNew.FlatAppearance.BorderSize = 2
+        Me.BtnNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnNew.Location = New System.Drawing.Point(4, 22)
+        Me.BtnNew.Name = "BtnNew"
+        Me.BtnNew.Size = New System.Drawing.Size(75, 23)
+        Me.BtnNew.TabIndex = 1
+        Me.BtnNew.Text = "New"
+        Me.BtnNew.UseVisualStyleBackColor = True
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.BtnEdit.FlatAppearance.BorderSize = 2
+        Me.BtnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnEdit.Location = New System.Drawing.Point(85, 22)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEdit.TabIndex = 2
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
+        '
+        'BtnRefresh
+        '
+        Me.BtnRefresh.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.BtnRefresh.FlatAppearance.BorderSize = 2
+        Me.BtnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRefresh.Location = New System.Drawing.Point(247, 22)
+        Me.BtnRefresh.Name = "BtnRefresh"
+        Me.BtnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRefresh.TabIndex = 4
+        Me.BtnRefresh.Text = "Refresh"
+        Me.BtnRefresh.UseVisualStyleBackColor = True
+        '
+        'BtnRemove
+        '
+        Me.BtnRemove.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.BtnRemove.FlatAppearance.BorderSize = 2
+        Me.BtnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRemove.Location = New System.Drawing.Point(166, 22)
+        Me.BtnRemove.Name = "BtnRemove"
+        Me.BtnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRemove.TabIndex = 3
+        Me.BtnRemove.Text = "Remove"
+        Me.BtnRemove.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(602, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(198, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'BtnSearch
+        '
+        Me.BtnSearch.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.BtnSearch.FlatAppearance.BorderSize = 2
+        Me.BtnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnSearch.Location = New System.Drawing.Point(516, 22)
+        Me.BtnSearch.Name = "BtnSearch"
+        Me.BtnSearch.Size = New System.Drawing.Size(80, 23)
+        Me.BtnSearch.TabIndex = 6
+        Me.BtnSearch.Text = "Search"
+        Me.BtnSearch.UseVisualStyleBackColor = True
+        '
+        'UserForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 423)
+        Me.Controls.Add(Me.BtnSearch)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.BtnRefresh)
+        Me.Controls.Add(Me.BtnRemove)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnNew)
+        Me.Controls.Add(Me.UserGrid)
+        Me.Name = "UserForm"
+        Me.Text = "UserForm"
+        CType(Me.UserGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UserGrid As DataGridView
+    Friend WithEvents BtnNew As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents BtnRefresh As Button
+    Friend WithEvents BtnRemove As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BtnSearch As Button
+End Class
---
+++ DepartmentalStore/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class UserForm
+    Dim DataContext As New DSMSEntities
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim lst As New List(Of spGetAllFromUser_Result)
+        lst = DataContext.spGetAllFromUser().ToList()
+        UserGrid.DataSource = lst
+        'For Each item As String In UserGrid.Columns(0).HeaderText.ToList()
+        '    If item.Contains("_") Then
+        '        UserGrid.Columns.Remove(item)
+        '        item.Replace("_", " ")
+        '        UserGrid.Columns.Add(item, item)
+        '    Else
+        '        UserGrid.Columns.Add(item, item)
+        '    End If
+        'Next
+    End Sub
+
+    Private Sub BtnNew_Click(sender As Object, e As EventArgs) Handles BtnNew.Click
+
+    End Sub
+End Class
